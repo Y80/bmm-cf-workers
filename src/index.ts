2876ab361f496d90b6d6b31a535ac36680dfe954
zf <lingchuan.zf@raycloud.com>
2026-05-12 18:28:04 +0800

    feat: add /md endpoint returning API list as plain markdown

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from 'hono'
 import home from './routes/index'
 import testUrl from './routes/test-url'
 import fetchHtml from './routes/fetch-html'
+import md from './routes/md'
 
 const app = new Hono()
 
 app.route('/', home)
 app.route('/', testUrl)
 app.route('/', fetchHtml)
+app.route('/', md)
 
 export default app
